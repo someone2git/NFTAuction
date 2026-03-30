e4a2fcc48c56249724e7da3991cac76b631ec05b
someone2git <ultra126wangyi@126.com>
2026-03-30 12:18:39 +0800

    mock 测试代理

---
+++ contracts/mocks/MockNFT.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockNFT is ERC721 {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("MockNFT", "MNFT") {}
+
+    function mint(address to) external returns (uint256) {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+        _mint(to, tokenId);
+        return tokenId;
+    }
+
+    function mintBatch(address to, uint256 amount) external {
+        for (uint256 i = 0; i < amount; i++) {
+            uint256 tokenId = _tokenIdCounter;
+            _tokenIdCounter++;
+            _mint(to, tokenId);
+        }
+    }
+}
